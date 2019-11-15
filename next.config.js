a4e3193306d85a162c6694a56c86feb40fb37a2a
Alex Keramidas <dev.alexkeramidas@gmail.com>
2019-11-15 19:12:09 +0200

    Added config for local

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-const bucket = () => `time2l-lambda-approvals-${process.env.NODE_ENV === 'dev' ? 'development' : process.env.NODE_ENV}`;
+const bucket = () =>
+    process.env.NODE_ENV !== 'local'
+        ? `https://s3.eu-central-1.amazonaws.com/time2l-lambda-approvals-${
+              process.env.NODE_ENV === 'dev'
+                  ? 'development'
+                  : process.env.NODE_ENV
+          }`
+        : '';
 
 module.exports = {
-    assetPrefix: `https://s3.eu-central-1.amazonaws.com/${bucket()}`,
+    assetPrefix: bucket(),
     target: 'serverless'
 };
