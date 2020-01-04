4bed4300fe1691293cb16829486f4b760e8b2cb5
Alex Keramidas <dev.alexkeramidas@gmail.com>
2020-01-04 18:59:28 +0200

    Added favicon?

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -23,6 +23,11 @@ export default class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                     />
+                    <link
+                        rel="shortcut icon"
+                        type="image/x-icon"
+                        href="/static/icons/favicon.ico"
+                    />
                 </Head>
                 <body>
                     <Main />
